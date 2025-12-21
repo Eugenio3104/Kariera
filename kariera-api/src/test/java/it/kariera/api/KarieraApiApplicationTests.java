9736f2e5f92cb00ad212b1c88140b3cc3446ab32
eug <grecoe02@gmail.com>
2025-12-21 19:03:13 +0100

    primo commit , setup iniziale del progetto , inizializzazione del frontend angular e configurazione sicura delle credenziali database tramite variabile d'ambiente

---
+++ kariera-api/src/test/java/it/kariera/api/KarieraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.kariera.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarieraApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
