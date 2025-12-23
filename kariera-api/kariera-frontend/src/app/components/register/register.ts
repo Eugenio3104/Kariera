8187541f3738d1243013a9111a7822ba96c4e245
eug <grecoe02@gmail.com>
2025-12-23 16:53:10 +0100

    Implementazione della logica di registrazione e login con binding bidirezionale, validazione dei form e gestione

--- kariera-api/kariera-frontend/src/app/components/register/register.ts
+++ kariera-api/kariera-frontend/src/app/components/register/register.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import {User} from '../../models/user.model';
+import {AuthService} from '../../services/auth';
+import {Router, RouterLink} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, CommonModule , RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class RegisterComponent {
 
+  user:User = {name:'', surname:'', email:'', password:''};
+
+  constructor(private authService: AuthService ,private router: Router) { }
+
+  onRegister() {
+    this.authService.registration(this.user).subscribe({
+      next: (res) => {
+        alert('Registrazione completata con successo!');
+        this.router.navigate(['/courses']);
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Errore nella registrazione: ' + (err.error || 'Riprova più tardi'));
+      }
+    });
+  }
 }
