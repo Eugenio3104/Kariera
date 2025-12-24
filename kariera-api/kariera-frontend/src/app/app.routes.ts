2e749dff8a6dd00e01d9d714900548f1cde88a74
eug <grecoe02@gmail.com>
2025-12-24 10:22:56 +0100

    Aggiunto componente "Courses" con routing protetto da authGuard.

--- kariera-api/kariera-frontend/src/app/app.routes.ts
+++ kariera-api/kariera-frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { RegisterComponent } from './components/register/register';
+import { CoursesComponent } from './components/courses/courses';
+import {authGuard} from './guards/auth.guard';
 
 export const routes: Routes = [
   {path: 'login' , component : LoginComponent},
   {path: 'registration' , component : RegisterComponent},
+  {path: 'courses' , component : CoursesComponent , canActivate: [authGuard]},
   {path: '' , redirectTo: '/login', pathMatch: 'full'}
 ];
