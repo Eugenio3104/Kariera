de095e5d5afe24202230a1c7a2d222410b809199
eug <grecoe02@gmail.com>
2025-12-26 11:16:48 +0100

    Aggiunti modelli Java StudyArea, Course e UserCourseSelection

---
+++ kariera-api/src/main/java/it/kariera/api/model/StudyArea.java
@@ -0,0 +1,14 @@
+package it.kariera.api.model;
+
+public class StudyArea {
+    private int id;
+    private String name;
+
+    public StudyArea(){}
+
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+}
